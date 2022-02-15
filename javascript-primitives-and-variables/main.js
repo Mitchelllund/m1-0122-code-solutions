e794bce381aa36633a4c8f223f7daa68e87055b6
Mitchell Lund <mitchlund922@gmail.com>
2022-02-14 19:37:25 -0800

    Completed Javascript-primitives-and-variables.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,16 @@
+var fullName = 'Mitchell Lund';
+var isCool = true;
+var totalPets = 1;
+var unknown = null;
+var never =
+
+console.log('value of fullName', fullName);
+console.log('value of', isCool);
+console.log('value of', totalPets);
+console.log('typeof fullName', typeof fullName);
+console.log('typeof isCool', typeof isCool);
+console.log('typeof totalPets', typeof totalPets);
+console.log('value of unknown: ', unknown);
+console.log('typeof unknown: ', typeof unknown);
+console.log('value of never', never);
+console.log('typeof never', typeof never);
